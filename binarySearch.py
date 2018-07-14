1d3929075c663bb29259bb0d13e64c03b3ca21fa
rohitn12 <33992841+rohitn12@users.noreply.github.com>
2018-07-13 20:19:36 -0500

    added iterative way

--- binarySearch.py
+++ binarySearch.py
@@ -10,5 +10,19 @@ def binarySearch(array , X, first , last ):
     else:
         return -1
 
+def search(self, nums, target):
+    left = 0
+    right = len(nums) - 1
+    mid = (left + right) // 2
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif (nums[mid] > target):
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
 #test         
 binarySearch([ 2, 3, 4, 10, 40 ] , 10 , 0 ,4 )
